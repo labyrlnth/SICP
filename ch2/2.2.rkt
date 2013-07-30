879819c573f94e570a595d07285c33726ef346db
labyrlnth <yu.kang2011@gmail.com>
2013-07-30 13:02:23 -0400

    add 2.2

---
+++ ch2/2.2.rkt
@@ -0,0 +1,27 @@
+;given
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (make-point x y)(cons x y))
+(define (x-point p)(car p))
+(define (y-point p)(cdr p))
+
+(define (make-segment p1 p2)(cons p1 p2))
+(define (start-segment l)(car l))
+(define (end-segment l)(cdr l))
+
+(define (average x y)(/ (+ x y) 2))
+
+(define (midpoint-segment l)
+  (make-point(average(x-point (start-segment l))
+                     (x-point (end-segment l)))
+             (average(y-point (start-segment l))
+                     (y-point (end-segment l)))))
+
+;test
+;(print-point (midpoint-segment (make-segment(make-point 1 1)(make-point 3 3))))
