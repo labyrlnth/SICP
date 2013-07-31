686592ff5c10ce6e3de1bf44a32cdcd55f8f0dc2
labyrlnth <yu.kang2011@gmail.com>
2013-07-31 16:46:24 -0400

    add 2.06

---
+++ ch2/2.06.rkt
@@ -0,0 +1,6 @@
+;given
+(define zero(lambda(f) (lambda(x)x)))
+(define (add-1 n)(lambda(f) (lambda(x) (f((n f)x)))))
+;by substitution
+(define one (lambda(f)(lambda(x)(f x))))
+(define two (lambda(f)(lambda(x)(f(f x)))))
